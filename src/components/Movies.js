2351be380bc0f6508c2a53a4de33ded19043634f
Kara Odle <karajo.odle@gmail.coml>
2019-11-06 10:20:57 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,28 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const mappedMovies = () => {
+    return movies.map((m, i) => {
+      return (
+        <div key={i}>
+          <h2>{m.title}</h2>
+          <h3>Runtime: {m.time} minutes</h3>
+          <h3>Genres</h3>
+          <ul>{m.genres.map((g, i) => {
+            return (
+              <li key={i}>{g}</li>
+            )
+          })}</ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {mappedMovies()}
     </div>
   );
 };
