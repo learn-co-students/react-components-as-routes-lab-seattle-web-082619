6d184e5de110f5e99af9ebdbd63fe1494eb18732
Krista Loven <krista.loven.87@gmail.com>
2019-11-06 10:53:24 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const displayActors = () => {
+    return actors.map((actor, idx) => (
+      <div key={idx}>
+        <h3>{actor.name}</h3>
+        <p>{actor.movies.join(', ')}</p>
+      </div>
+    ))
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {displayActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const displayMovies = () => {
+    return movies.map((movie, idx) => (
+      <div key={idx}>
+        <h2>{movie.title}</h2>
+        <p>{movie.time}</p>
+        <p>{movie.genres.join(', ')}</p>
+      </div>
+    ))
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {displayMovies()}
     </div>
   );
 };
